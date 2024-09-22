caf8da17c874fa01a456a20e0e1554bdf3451e2c
Jacek Skarupa <jacek.skarupa@mindsailors.com>
2024-09-22 23:14:24 +0200

    added working stuff from previous excavator firmware

--- rc_excavator_arduino_ide.ino
+++ rc_excavator_arduino_ide.ino
@@ -1,6 +1,46 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include "Wire.h"
+#include "Adafruit_PWMServoDriver.h"
 #include <Bluepad32.h>
 
+enum Actuators {
+  LEFT_DRIVE = 1,
+  RIGHT_DRIVE,
+  SWING_MOTOR,
+  ARM,
+  DIPPER,
+  BUCKET,
+  THUMB,
+  AUX,
+};
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 ControllerPtr myControllers[BP32_MAX_GAMEPADS];
+
+const uint8_t motor_1_A = 0;
+const uint8_t motor_1_B = 1;
+
+const uint8_t motor_2_A = 2;
+const uint8_t motor_2_B = 3;
+
+const uint8_t motor_3_A = 4;
+const uint8_t motor_3_B = 5;
+
+const uint8_t motor_4_A = 6;
+const uint8_t motor_4_B = 7;
+
+const uint8_t motor_5_A = 8;
+const uint8_t motor_5_B = 9;
+
+const uint8_t motor_6_A = 10;
+const uint8_t motor_6_B = 11;
+
+const uint8_t motor_7_A = 12;
+const uint8_t motor_7_B = 13;
+
+const uint8_t motor_8_A = 14;
+const uint8_t motor_8_B = 15;
 
 // This callback gets called any time a new gamepad is connected.
 // Up to 4 gamepads can be connected at the same time.
@@ -57,17 +97,13 @@ void dumpGamepad(ControllerPtr ctl) {
         ctl->miscButtons(),  // bitmask of pressed "misc" buttons
         //
         ctl->l1(),
-        // ctl->l2(),
         ctl->r1(),
-        // ctl->r2(),
         ctl->b()
     );
 }
 
 void processGamepad(ControllerPtr ctl) {
     if (ctl->x()) {
-        // Some gamepads like DS3, DS4, DualSense, Switch, Xbox One S, Stadia support rumble.
-        // It is possible to set it by calling:
         // Some controllers have two motors: "strong motor", "weak motor".
         // It is possible to control them independently.
         ctl->playDualRumble(0 /* delayedStartMs */, 250 /* durationMs */, 0x80 /* weakMagnitude */,
@@ -106,45 +142,116 @@ void processControllers() {
     }
 }
 
-// Arduino setup function. Runs in CPU 1
-void setup() {
-    Serial.begin(115200);
-    Serial.printf("Firmware: %s\n", BP32.firmwareVersion());
-    const uint8_t* addr = BP32.localBdAddress();
-    Serial.printf("BD Addr: %2X:%2X:%2X:%2X:%2X:%2X\n", addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
-
-    // Setup the Bluepad32 callbacks
-    BP32.setup(&onConnectedController, &onDisconnectedController);
-
-    // "forgetBluetoothKeys()" should be called when the user performs
-    // a "device factory reset", or similar.
-    // Calling "forgetBluetoothKeys" in setup() just as an example.
-    // Forgetting Bluetooth keys prevents "paired" gamepads to reconnect.
-    // But it might also fix some connection / re-connection issues.
-    // BP32.forgetBluetoothKeys();
-
-    // Enables mouse / touchpad support for gamepads that support them.
-    // When enabled, controllers like DualSense and DualShock4 generate two connected devices:
-    // - First one: the gamepad
-    // - Second one, which is a "virtual device", is a mouse.
-    // By default, it is disabled.
-    BP32.enableVirtualDevice(false);
+void drive_motor(uint8_t pinA, uint8_t pinB, uint8_t direction, uint16_t speed) {
+  if (direction == 0) {
+    pwm.setPWM(pinA, speed, 0);
+    pwm.setPWM(pinB, 0, 4096);
+  }
+  else {
+    pwm.setPWM(pinA, 0, 4096);
+    pwm.setPWM(pinB, speed, 0);
+  }
 }
 
-// Arduino loop function. Runs in CPU 1.
+void drive_actuator(Actuators motor, uint8_t direction, uint16_t speed) {
+  switch (motor)
+  {
+  case LEFT_DRIVE:
+    drive_motor(motor_1_A, motor_1_B, direction, speed);
+    Serial.println("LEFT_DRIVE");
+
+    break;
+  case RIGHT_DRIVE:
+    drive_motor(motor_2_A, motor_2_B, direction, speed);
+
+    break;
+  case SWING_MOTOR:
+    drive_motor(motor_3_A, motor_3_B, direction, speed);
+
+    break;
+  case ARM:
+    drive_motor(motor_4_A, motor_4_B, direction, speed);
+
+    break;
+  case DIPPER:
+    drive_motor(motor_5_A, motor_5_B, direction, speed);
+
+    break;
+  case BUCKET:
+    drive_motor(motor_6_A, motor_6_B, direction, speed);
+
+    break;
+  case THUMB:
+    drive_motor(motor_7_A, motor_7_B, direction, speed);
+
+    break;
+  case AUX:
+    drive_motor(motor_8_A, motor_8_B, direction, speed);
+    
+    break;
+  
+  default:
+    break;
+  }
+}
+
+// Arduino setup function. Runs in CPU 1
+void setup() {
+  Serial.begin(115200);
+  Serial.printf("Firmware: %s\n", BP32.firmwareVersion());
+  const uint8_t* addr = BP32.localBdAddress();
+  Serial.printf("BD Addr: %2X:%2X:%2X:%2X:%2X:%2X\n", addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
+
+  // Setup the Bluepad32 callbacks
+  BP32.setup(&onConnectedController, &onDisconnectedController);
+
+  // "forgetBluetoothKeys()" should be called when the user performs
+  // a "device factory reset", or similar.
+  // Calling "forgetBluetoothKeys" in setup() just as an example.
+  // Forgetting Bluetooth keys prevents "paired" gamepads to reconnect.
+  // But it might also fix some connection / re-connection issues.
+  // BP32.forgetBluetoothKeys();
+
+  // Enables mouse / touchpad support for gamepads that support them.
+  // When enabled, controllers like DualSense and DualShock4 generate two connected devices:
+  // - First one: the gamepad
+  // - Second one, which is a "virtual device", is a mouse.
+  // By default, it is disabled.
+  BP32.enableVirtualDevice(false);
+}
+
 void loop() {
-    // This call fetches all the controllers' data.
-    // Call this function in your main loop.
-    bool dataUpdated = BP32.update();
-    if (dataUpdated)
-        processControllers();
+  // This call fetches all the controllers' data.
+  // Call this function in your main loop.
+  bool dataUpdated = BP32.update();
+  if (dataUpdated)
+      processControllers();
 
-    // The main loop must have some kind of "yield to lower priority task" event.
-    // Otherwise, the watchdog will get triggered.
-    // If your main loop doesn't have one, just add a simple `vTaskDelay(1)`.
-    // Detailed info here:
-    // https://stackoverflow.com/questions/66278271/task-watchdog-got-triggered-the-tasks-did-not-reset-the-watchdog-in-time
+  // The main loop must have some kind of "yield to lower priority task" event.
+  // Otherwise, the watchdog will get triggered.
+  // If your main loop doesn't have one, just add a simple `vTaskDelay(1)`.
+  // Detailed info here:
+  // https://stackoverflow.com/questions/66278271/task-watchdog-got-triggered-the-tasks-did-not-reset-the-watchdog-in-time
 
-    //     vTaskDelay(1);
-    delay(150);
+  //     vTaskDelay(1);
+  delay(150);
+
+  // drive_actuator(LEFT_DRIVE, 0, 2048);
+  // drive_actuator(RIGHT_DRIVE, 0, 2048);
+  // drive_actuator(SWING_MOTOR, 0, 2048);
+  // drive_actuator(ARM, 0, 2048);
+  // drive_actuator(DIPPER, 0, 2048);
+  // drive_actuator(BUCKET, 0, 2048);
+  // drive_actuator(THUMB, 0, 2048);
+  // drive_actuator(AUX, 0, 2048);
+  // delay(1000);
+  // drive_actuator(LEFT_DRIVE, 1, 2048);
+  // drive_actuator(RIGHT_DRIVE, 1, 2048);
+  // drive_actuator(SWING_MOTOR, 1, 2048);
+  // drive_actuator(ARM, 1, 2048);
+  // drive_actuator(DIPPER, 1, 2048);
+  // drive_actuator(BUCKET, 1, 2048);
+  // drive_actuator(THUMB, 1, 2048);
+  // drive_actuator(AUX, 1, 2048);
+  // delay(1000);
 }
